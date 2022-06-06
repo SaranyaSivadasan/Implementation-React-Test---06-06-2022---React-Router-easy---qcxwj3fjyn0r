f544ca8ef21af6848de891b6cce9dda2acf366a0
SaranyaSivadasan <68867201+SaranyaSivadasan@users.noreply.github.com>
2022-06-06 21:36:19 +0530

    Implementation React Test

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Index from "../src/Pages/Index";
+import Home from "../src/Pages/Home";
+import NotFound from "../src/Pages/NotFound";
 
 export const AppRoutes = () => {
-    return (
-
-    )
-}
+  return (
+    <HashRouter>
+      <switch>
+        <Route exact path="/" component={Index} />
+        <Route exact path="/home" component={Home} />
+        <Route path="/*" component={NotFound} />
+      </switch>
+    </HashRouter>
+  );
+};
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React from "react";
 
-
-export const NavBar = () =>{
-    return(
-
-    )
-}
+export const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a class="index-link" href="/">
+            First
+          </a>
+        </li>
+        <li>
+          <a class="home-link" href="/home">
+            Second
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
